83e0f6e03016e8c17c5882e795c9d53cccb244e7
Eashan Srivastava <75667274+Ventmaster@users.noreply.github.com>
2023-06-25 18:58:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var countDownDate = new Date ("Jan 01, 2024 00:00:00").getTime();
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance/(1000*60*60*24));
+    var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+    var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+    var seconds = Math.floor((distance%(1000*60))/1000);
+
+    document.getElementById("launch").innerHTML = days + "D " + hours + "H " + minutes + "M " + seconds + "S";
+
+    if (distance < 0){
+        clearInterval(x);
+        document.getElementById('launch').innerHTML="EXPIRED";
+    }
+}, 1000);
+
+// END OF Script.JS FILE
